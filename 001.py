f5c2ecd4251e172843d6c7c0b6143c171ace5a0c
Daniel David <ddavid37@montgomerycollege.edu>
2024-11-25 12:15:03 -0500

    Remove 001.py file containing version checks for imageio, matplotlib, and numpy

--- 001.py
+++
@@ -1,7 +0,0 @@
-import imageio.v3 as iio
-import matplotlib
-import numpy as np
-
-print("imageio version:", iio.__version__)
-print("matplotlib version:", matplotlib.__version__)
-print("numpy version:", np.__version__)
